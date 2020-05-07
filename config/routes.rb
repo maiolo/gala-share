864936dd5d9cb36812d3107f87a59202161b863d
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-05-07 20:34:27 -0300

    User controller with routes deals

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users do 
-    resources :deals, only: [:index, :show, :edit, :update]
+  devise_for :users 
+  root to: "pages#home" 
+  get "/dashboard", to: 'pages#dashboard' 
+  resources :users, only: [ :show, :edit ] do 
+    resources :deals, only: [ :index, :show, :edit, :update ]
   end
-  root to: "pages#home"
-  resources :products
+  resources :products 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
