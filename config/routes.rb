618ad5d9164fee69d684ae72dcb331ff03c26097
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-05-07 19:51:42 -0300

    Created model for Deals, controllers for Deals and Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do 
+    resources :deals, only: [:index, :show, :edit, :update]
+  end
   root to: "pages#home"
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
