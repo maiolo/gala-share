0eac47503f471acf6f442657cf589b691c95b095
Lucas Lima <lucasrlima@gmail.com>
2020-05-09 12:44:24 -0300

    images added to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
 
+  has_many_attached :photos
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :size, presence: true
+  validates :photos, presence: true
 end
