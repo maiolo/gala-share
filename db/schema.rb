e371fffc18f0699978167d1928c7596e1f764efe
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-05-12 20:15:23 -0300

    removes type from deals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_230828) do
+ActiveRecord::Schema.define(version: 2020_05_12_231352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_05_12_230828) do
   end
 
   create_table "deals", force: :cascade do |t|
-    t.string "type"
     t.bigint "users_id", null: false
     t.bigint "products_id", null: false
     t.datetime "created_at", precision: 6, null: false
